91bef6ea052ab7ace430dfd5a039e67a087ad632
ludo <ludovic.duvaux@univ-angers.fr>
2016-05-30 11:39:15 +0200

    add 'get.np.pval.R'

---
+++ stats_math/get.np.pval.R
@@ -0,0 +1,21 @@
+get_pval <- function(obs, exp.dist, two_sided=F, greater=T){
+# function to compute a p-value from an experimental distribution (to be provided)
+# obs: observed value of the statistics
+# exp.dist: experimental distribution of the statistics
+# two_sided: boolean
+# greater: if two_sided=F, H1 is obs is greater than expected.
+
+    if ( greater | !two_sided ) {
+        prop <- 1-length(subset(exp.dist, obs >= exp.dist)
+    } else {
+        prop <- 1-length(subset(exp.dist, obs <= exp.dist)
+    }
+
+    pval <- prop)/length(exp.dist)
+    
+    if (two_sided) {
+        pval2 <- abs(1-pval)
+        pval <- ifelse(pval<=pval2, pval, pval2)*2}
+    
+    return(pval)
+}
